6bf5c5b89ef20bfbd1bcc236708323045ea49dfd
EmJo <emily_a_johnson@hotmail.com>
2016-07-21 14:33:57 +0000

    added paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160721141803) do
 
   create_table "comments", force: :cascade do |t|
