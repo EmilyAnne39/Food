2de4164ecaefce0149dd96675328d8a0bd1ab13a
EmJo <emily_a_johnson@hotmail.com>
2016-07-21 14:14:15 +0000

    paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721140033) do
+ActiveRecord::Schema.define(version: 20160721140920) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20160721140033) do
     t.text     "recipe_instructions"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
