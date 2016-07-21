7cd13c769df4c67a5ad7a02d9b1fe353adf349f4
Peter Baggish <p.baggish@gmail.com>
2016-07-21 09:56:02 -0400

    added model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+
+  get 'recipes/show'
+
+  get 'recipes/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ db/migrate/20160721135315_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :category
+      t.text :recipe_ingredients
+      t.integer :rating
+      t.text :recipe_instructions
+
+      t.timestamps null: false
+    end
+  end
+end
